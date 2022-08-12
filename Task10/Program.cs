a4e75fb223d52d2deae2a2a3ba64d581e52e7874
EfimovE <efimov_e@mail.ru>
2022-08-12 19:41:23 +0500

    save1

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число и нажмите Enter.");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 >= 1 && number / 100 <= 9) Console.WriteLine($"{number} -> Трехзначное число.");
+else Console.WriteLine($"Вы ввели не трехзначное число.");
