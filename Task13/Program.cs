01f07136b9b98a863a40b43321b010782d0b0791
EfimovE <efimov_e@mail.ru>
2022-08-13 21:38:21 +0500

    save2

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+int thirdDigital (int num)
+{
+    if (num < 100) return -1;
+    {
+        while (num/100 > 9) num/=10;
+        return num % 10;
+    }
+}
+Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = thirdDigital (number);
+string res = result == -1 ? "Третьей цифры нет." : result.ToString();
+Console.WriteLine($"{res}");
